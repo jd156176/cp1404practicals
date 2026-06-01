9f0c49fb961936bf44a5eee91db85b7cdfb40f9c
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-06-01 15:13:38 +1000

    Add docstring for functions, improve function names

--- prac_02/score.py
+++ prac_02/score.py
@@ -6,15 +6,16 @@ import random
 
 def main():
     score = float(input("Enter score: "))
-    print(f"User score {score} is {calculate_result(score)}")
+    print(f"User score {score} is {class_result(score)}")
 
     if score == 100:
         print("You Get a Prize!")
 
     random_score = random.randint(0,100)
-    print(f"Random: {random_score} is {calculate_result(random_score)}")
+    print(f"Random: {random_score} is {class_result(random_score)}")
 
-def calculate_result(score: float):
+def class_result(score: float):
+    """Class result based on score threshold."""
     if score < 0 or score > 100:
         return "Invalid Score"
     elif score >= 90:
