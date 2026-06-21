a92090ab2ef01650f440e6c4500895bb7ad9f2c9
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-06-21 22:29:39 +0900

    add random line generating and formatting

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,11 +1,15 @@
 """Module 4 Lottery Ticket Generator"""
 import random
 
+NUMBERS_PER_LINE = 6
+LOWEST_NUMBER = 1
+HIGHEST_NUMBER = 45
+
 def main():
-    NUMBERS_PER_LINE = 6
-    LOWEST_NUMBER = 1
-    HIGHEST_NUMBER = 45
     number_of_lines = int(input("How many quick picks? "))
 
-    quick_pick = []
-    for number
+    for line in range(number_of_lines):
+        random_numbers = [random.randint(LOWEST_NUMBER, HIGHEST_NUMBER) for number in range(NUMBERS_PER_LINE)]
+        print(" ".join(f"{number:2}" for number in random_numbers))
+
+main()
