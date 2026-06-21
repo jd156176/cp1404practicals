4eb54522758456ffc6f280e5045f3ec286d8cd0f
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-06-21 11:08:43 +1000

    add print output and refactor variable names

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,14 @@ FILENAME = "subject_data.txt"
 
 def main():
     """Program to load and display subject data from file."""
-    data = load_data(FILENAME)
-    print(data)
+    subject_data = load_data(FILENAME)
+    for subject, lecturer, number_of_students in subject_data:
+        print(f"{subject} is taught by {lecturer} and has {number_of_students} students")
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    all_subject_data = []
     input_file = open(filename)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,10 +24,13 @@ def load_data(filename=FILENAME):
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         # Make the number an integer as part of a new, poorly named, list
-        data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
+        subject_data = [parts[0], parts[1], int(parts[2])]
+        all_subject_data.append(subject_data)
+        print(subject_data)  # See if that worked
         print("----------")
     input_file.close()
+
+    return all_subject_data
 
 
 main()
