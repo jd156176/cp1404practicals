01ab4187018dc7af8b9c1752fab5473582afcf4a
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-06-21 11:22:40 +1000

    add woeful password checker, refactor functions

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,14 +1,34 @@
 """Week 4 List exercise program"""
+from typing import Any
+
 
 def main():
     numbers = []
     for i in range(5):
         number = float(input("Number: "))
         numbers.append(number)
+    print_number_information(numbers)
+
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Please type username: ")
+    check_username(username, usernames)
+
+
+def print_number_information(numbers: list[Any]):
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
-    print(f"The average of the numbers is {(sum(numbers)/len(numbers)):.2f}")
+    print(f"The average of the numbers is {(sum(numbers) / len(numbers)):.2f}")
+
+
+def check_username(username: str, usernames: list[Any]):
+    if username in usernames:
+        print("Access Granted")
+    else:
+        print("Access Denied")
+
 
 main()
