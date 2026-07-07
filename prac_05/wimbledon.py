029533c72f26e4ec117dc02ec33a7f77dea28969
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-07-07 13:36:28 +1000

    complete prac 5 wimbledon exercise

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,7 +1,7 @@
 """
 Wimbledon
 Estimated Time: 45 minutes
-Actual Time:
+Actual Time: 40 minutes
 """
 
 FILENAME = "wimbledon.csv"
@@ -12,23 +12,39 @@ def load_file(filename):
     """Load the data from the CSV file."""
     champions = []
     with open(filename, "r", encoding = "utf-8-sig") as in_file:
+        in_file.readline()  # Remove CSV header row
         for line in in_file:
-            parts = line.strip().split("")
-
-            champion_parts = [parts[CHAMPION_COUNTRY_COLUMN], parts[CHAMPION_NAME_COLUMN]]
-            champions.append(champion_parts)
+            parts = line.strip().split(",")
+            champions.append(parts)
 
     return champions
 
 def main():
     """Run the main Wimbledon program."""
-    load_file(FILENAME)
+    champions = load_file(FILENAME)
+    champion_to_count, countries = calculate_champion_counts(champions)
+    display_results(champion_to_count, countries)
 
 def calculate_champion_counts(champions):
     """Calculate a dictionary of champion names to number of wins."""
     champion_to_count = {}
     countries = set()
-    for country, name in champions:
-        champion_to_count[name] = champion_to_count.get(name, 0) + 1
+    for champion in champions:
+        countries.add(champion[CHAMPION_COUNTRY_COLUMN])
+        try:
+            champion_to_count[champion[CHAMPION_NAME_COLUMN]] += 1
+        except KeyError:
+            champion_to_count[champion[CHAMPION_NAME_COLUMN]] = 1
+    return champion_to_count, countries
+
+def display_results(champion_to_count, countries):
+    """Displays Champions and Countries."""
+    print("Wimbledon Champions: ")
+
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print()
+    print(f"These {len(countries)} have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
 
 main()
