e2d5c0d93eee071d52aa2a5d572c36b73f5b743e
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-07-19 20:48:41 +1000

    add doc-string, refactor is_dynamic function to correctly check status.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,4 +1,5 @@
-"""Cp1404 Week 6 Practical - process programing languages
+"""
+Cp1404 Week 6 Practical - process programing languages
 
 Estimated Time: 20 Minutes
 Actual Time: 16 Minutes 38 Seconds
@@ -8,13 +9,22 @@ class ProgrammingLanguage:
     """Represent a programming language object."""
 
     def __init__(self, name="", typing="static",reflection=True,year=0):
+        """
+        Initialize a programming language instance
+        :param name: string, name of programming language
+        :param typing: string, programming typing mode, either static or dynamic
+        :param reflection: boolean
+        :param year: int, year of programming language conception
+        """
         self.name = name
         self.typing = typing
         self.reflection =  reflection
         self.year = year
 
     def is_dynamic(self):
-        return self.reflection
+        """Returns true if program is dynamically typed, false if program is static"""
+        return self.typing.upper() == "DYNAMIC"
 
     def __str__(self):
+        """Print the program name, typing method, reflection status and language conception year."""
         return f"{self.name}, {self.typing} typing, Reflection={self.reflection}, First appeared in {self.year}"
