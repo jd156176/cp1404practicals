45ad957eab4c01b41544c975cad769d5abd79b97
jd156176 <charlotte.eime@my.jcu.edu.au>
2026-08-02 14:59:03 +1000

    Add load_file function

---
+++ prac_07/project.py
@@ -0,0 +1,18 @@
+import datetime
+
+class Project:
+    """Represent project details as an object"""
+
+    def __init__(self, name="", start_date=None, priority=0, cost_estimate =0.0, completion_percentage=0):
+        self.name = name
+        self.start_date = start_date #expects a date.time object
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return (f"{self.name} Start Date:{self.start_date} Priority:{self.priority} Cost Estimate:{self.cost_estimate}"
+                f"Completion Percentage:{self.completion_percentage}")
+
+    def __repr__(self):
+        return str(self)
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,3 +3,35 @@ CP1404 Prac 7 - Project Management Program
 Estimated Time: 1 hour
 Actual time:
 """
+import datetime
+from project import Project
+
+MENU = """-(L)oad projects
+-(S)ave projects
+-(D)isplay projects
+-(F)ilter projects by date
+-(A)dd new project
+-(U)pdate project
+-(Q)uit"""
+FILENAME = "projects.txt"
+
+def main():
+    projects = []
+    print("Welcome to Pythonic Project Management")
+    load_projects(FILENAME, projects)
+    print(MENU)
+    choice = input(">>>> ").upper()
+
+def load_projects(filename, projects: list[Project]):
+    with open(filename, "r") as in_file:
+        #file format: name, start date, priority, cost estimate, completion percentage
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split("\t")
+            date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+            projects.append(Project(parts[0], date, int(parts[2]), float(parts[3]), int(parts[4])))
+    print(f"Loaded {len(projects)} from {filename}")
+
+
+main()
+
